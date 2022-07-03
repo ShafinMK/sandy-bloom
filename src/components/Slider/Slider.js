89e122ba1d2e5139f6fdbda19d2a23737140c324
ShafinMK <dr.hulk30@gmail.com>
2022-07-03 10:45:46 +0600

    slider text size and color changed

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -14,7 +14,9 @@ const Slider = () => {
                         <img src="https://cdn.pixabay.com/photo/2017/01/28/19/31/landscape-2016308_960_720.jpg" className="d-block w-100 img-fluid vh-100 vw-100 position-relative  opacity-25" alt="..." />
                         
                         <div className="carousel-caption  d-block position-absolute top-50 start-50 translate-middle">
-                            <h1 className='permanent-marker-font'>MAKE YOURSELF AT HOME</h1>
+                            <h6>Lorem ipsum dolor sit.</h6>
+                            <h1 className='permanent-marker-font slider-large-txt'>MAKE YOURSELF <span className='bluish-font'>AT Home</span></h1>
+                            <h2>Lorem ipsum dolor sit amet consectetur.</h2>
                             <button className='btn bg-transparent text-warning border border-warning mt-5'>View Our Rooms</button>
 
                         </div>
@@ -24,7 +26,7 @@ const Slider = () => {
                         <div className="carousel-caption d-block position-absolute top-50 start-50 translate-middle">
                             {/* use Different font  */}
                             <h6>STAY WITH US FEEL LIKE HOME</h6>
-                            <h1 className='permanent-marker-font'>Redefine your expectations</h1>
+                            <h1 className='permanent-marker-font slider-large-txt'>Redefine Your <span className='greeny-text'>Expectations</span></h1>
                             {/* different font 3rd line */}
                             <h2 className='play-ball-font'>A hotel experience, unlike the rest</h2>
                             <button className='btn bg-transparent text-warning border border-warning mt-5'>Check Our Fascilities</button>
@@ -33,7 +35,9 @@ const Slider = () => {
                     <div className="carousel-item bg-black">
                         <img src="https://images.unsplash.com/photo-1567062691477-fcbc8bbdf5b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" className="d-block w-100 img-fluid vh-100 opacity-25" alt="..." />
                         <div className="carousel-caption d-block position-absolute top-50 start-50 translate-middle">
-                            <h1 className='permanent-marker-font'>Thermal Baths</h1>
+                            <h6>Lorem ipsum dolor sit amet consectetur.</h6>
+                            <h1 className='permanent-marker-font slider-large-txt'><span className='orange-text'>Thermal</span> Baths</h1>
+                            <h2>Lorem ipsum dolor sit amet.</h2>
                             <button className='btn bg-transparent text-warning border border-warning mt-5'>View Luxary Baths</button>
                         </div>
                     </div>
